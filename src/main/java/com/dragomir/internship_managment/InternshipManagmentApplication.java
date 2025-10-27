0681d50e5ee5cafe902db683ce04b1eb12d7650d
Drago Vlacic <dragomirvlacic@gmail.com>
2025-10-27 20:42:08 +0100

    inital commit, project setup

---
+++ src/main/java/com/dragomir/internship_managment/InternshipManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.dragomir.internship_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipManagmentApplication.class, args);
+	}
+
+}
